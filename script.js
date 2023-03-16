2be794304600124d55d7f5fd5c77d6d6a1836eea
damcio <d.p.koziol@gmail.com>
2023-03-16 12:24:51 +0100

    Board render function , players create function

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-const gameBoard = (() => {})();
+/* eslint-disable quotes */
+
+const gameBoard = (() => {
+  const gameBoardArr = ["", "", "", "", "", "", "", "", ""];
+
+  const createBoard = () => {
+    let boardElements = "";
+    gameBoardArr.forEach((field, index) => {
+      boardElements += `<div class="field" id="field-${index}">${field}</div>`;
+    });
+    document.querySelector(".gameboard").innerHTML = boardElements;
+  };
+
+  return {
+    createBoard,
+  };
+})();
+
+const createPlayer = (name, mark) => ({
+  name,
+  mark,
+});
+
+const Game = (() => {
+  let players;
+  let currentPlayer;
+  let gameOver;
+
+  const gameStart = () => {
+    players = [
+      createPlayer(document.querySelector("#playerName1").value, "X"),
+      createPlayer(document.querySelector("#playerName2").value, "O"),
+    ];
+    currentPlayer = 0;
+    gameOver = false;
+    gameBoard.createBoard();
+  };
+  return {
+    gameStart,
+  };
+})();
+
+const startButton = document.querySelector("#gameStart");
+startButton.addEventListener("click", () => {
+  Game.gameStart();
+});
